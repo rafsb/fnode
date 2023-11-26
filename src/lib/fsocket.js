3115b9036b588033bb40c7d95604a44f8ef7b5aa
r_sb <r_sb@live.com>
2023-11-26 13:09:06 -0300

    rooms are back and a global room is created by default to store all connections

--- src/lib/fsocket.js
+++ src/lib/fsocket.js
@@ -2,26 +2,22 @@ global.SOCKET = process.env.SOCKET || 9000
 
 const
 ws = require('ws')
-, io = require('./utils/fio')
-, fobject = require('./utils/fobject')
-, rooms = {}
+, io = require('./utils/io')
+, fobject = require('./utils/object')
 ;;
+global.fsockrooms = fobject.cast({ global: new Set() })
 
 module.exports = server => {
     const sockserver = new ws.Server({ server }) ;;
     sockserver.on('connection', socket => {
-        socket.on('close', _ => Object.values(rooms).forEach(room => room.delete(socket)))
+        
+        if(!fsockrooms[`global`].has(socket)) fsockrooms[`global`].add(socket)
+
+        socket.on('close', _ => Object.keys(fsockrooms).forEach(room => fsockrooms[room].has(socket) ? fsockrooms[room].delete(socket) : null))
         socket.on('message', async data => {
 
             try { data = JSON.parse(data) } catch(error) { data = null }
             let res = { emitter: data.emitter } ;;
-
-            if(data?.room) {
-                if(!rooms[data.room]) rooms[data.room] = new Set()
-                rooms[data.room].add(socket)
-                Array.from(rooms[data.room]).forEach(sock => sock != socket ? sock.send(JSON.stringify({ data: { clients: Array.from(rooms[data.room]).length } })) : null)
-                socket.send(JSON.stringify({ data: { status: true, clients: Array.from(rooms[data.room]).length } }))
-            }
 
             if(data?.path && data.path != '/') {
                 const
@@ -33,7 +29,7 @@ module.exports = server => {
                     if(io.exists(EPaths.CONTROLLERS + classname + '.js')) {
                         const
                         cls = require(EPaths.CONTROLLERS + classname)
-                        , args = fobject.blend({}, data, { params: path })
+                        , args = fobject.blend({}, data, { params: path }, { emit: r => socket.send(JSON.stringify(fobject.blend({}, { emitter: res.emitter }, { data: r }))) })
                         ;;
 
                         let tmp ;;
