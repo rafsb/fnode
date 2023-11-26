0beb6adf0fb2b3a25ad336b12ae03849bc8109f0
r_sb <r_sb@live.com>
2023-11-26 12:20:32 -0300

    new version notation new utils names using http instead of https added a global cache system

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * APP.JS
  *---------------------------------------------------------------------------------------------
  */
-process.env.UV_THREADPOOL_SIZE = require('os').cpus().length * 2
+process.env.UV_THREADPOOL_SIZE = require('os').cpus().length
 
 require('dotenv').config({ path: `./.env` })
 
@@ -10,8 +10,8 @@ global.HOST             = process.env.HOST
 global.PORT             = process.env.PORT             || 3000
 global.VERBOSE          = process.env.VERBOSE * 1      || 0
 global.DB_DOC_SIZE      = process.env.DB_DOC_SIZE * 1  || 1024
-global.VERSION          = process.env.VERSION          || "0.0.1-alpha"
-global.DB_DRIVER        = process.env.DB_DRIVER        || "fstore"
+global.VERSION          = process.env.VERSION          || "0.1-alpha"
+global.DB_DRIVER        = process.env.DB_DRIVER        || "iostore"
 global.DB_NAME          = process.env.DB_NAME          || "core"
 global.DB_USER          = process.env.DB_USER          || ""
 global.DB_PASSWORD      = process.env.DB_PASSWORD      || ""
@@ -19,14 +19,16 @@ global.DB_PK            = process.env.DB_PK            || ""
 global.DB_KEY           = process.env.DB_KEY           || ""
 global.DB_ENDPOINT      = process.env.DB_ENDPOINT      || ""
 global.DB_PORT          = process.env.DB_PORT          || ""
+global.DB_CACHE         = process.env.DB_CACHE * 1     || 0
 global.SESSION_DURATION = process.env.SESSION_DURATION || 24 * 60
 global.ROOT             = __dirname + '/src/'
 
 const
 path        = require('path')
-, fio       = require('./src/lib/utils/fio')
-, ftext     = require('./src/lib/utils/ftext')
-, fdate     = require('./src/lib/utils/fdate')
+, fio       = require('./src/lib/utils/io')
+, ftext     = require('./src/lib/utils/text')
+, fdate     = require('./src/lib/utils/date')
+, fcache     = require('./src/lib/utils/cache')
 , express   = require('express')
 , parser    = require('body-parser')
 , app       = express()
@@ -35,6 +37,7 @@ path        = require('path')
 , ca        = fio.read("etc/keys/csr.pem")
 ;;
 
+global.global_cache = fcache.load(`cache`)
 global.KEY = fio.read('/etc/keys/private.key')
 require('./src/lib/constants')
 
@@ -77,9 +80,9 @@ app.keepAliveTimeout = 0
 // *                 server settings                   *
 // *---------------------------------------------------*
 try {
-    const htserver = require("https").createServer({ key, cert }, app) ;;
-    // const htserver = require("http").createServer(app) ;;
-    require('./src/lib/fsocket')(htserver)
+    // global.htserver = require("https").createServer({ key, cert }, app) ;;
+    global.htserver = require("http").createServer(app) ;;
+    global.fsock = require('./src/lib/fsocket')(htserver)
     htserver.listen(PORT, _ => console.log(
         ETerminalColors.BG_BLUE
         + ETerminalColors.FT_WHITE
