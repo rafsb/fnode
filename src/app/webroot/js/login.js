7147f8311e73bf76068c566b1c7cf7118180163c
r_sb <r_sb@live.com>
2023-11-26 13:01:24 -0300

    new login

--- src/app/webroot/js/login.js
+++ src/app/webroot/js/login.js
@@ -1,28 +1,35 @@
 (function(app, args){
 
     const
-    d = DIV('row content-center').app(
-        TAG('form', 'col-2').attr({ action:'javascript:void(0)' }).app(
+    d = DIV('wrap relative').app([
+        TAG('form', 'col-2 centered', { background:`{{background}}`, padding:`1em`, borderRadius:`.5em`, boxShadow:`0 0 .5em {{dark3}}` }).attr({ action:'javascript:void(0)' }).app(
+            TAG('header', 'row', { marginBottom: `2em` }).app(
+                IMG('img/logo.png', 'left', { height:'3em', width:'3em', borderRadius:'50%', boxShadow:'0 0 1em {{dark2}}' })
+            ).app(
+                SPAN('LOGIN', 'col-6 right content-right', { color:'white', borderRadius:'0 1.5em 1.5em 0', padding:`1em 1.5em`, backgroundImage: 'linear-gradient(to left, {{belize_hole}}44, transparent)' })
+            )
+        ,
             DIV('row').app(
                 TAG('input', 'row').attr({ name:'user', type:'user', placeholder:'Usuário' })
             )
-        ).app(
+        ,
             DIV('row', { margin: "1em 0"}).app(
                 TAG('input', 'row -hash').attr({ name:'pswd', type:'password', placeholder:'Senha' })
             )
-        ).app(
+        ,
             DIV('row').app(
                 TAG('input', 'row only-pointer', {
-                    marginTop: "1em"
-                    , background:'{{GREEN_SEA}}'
-                    // , color: '{{SILVER}}'
-                }).attr({ type:'submit', value:'Pronto!' }).on('click', function() {
+                    background:'{{belize_hole}}'
+                    , color: 'white'
+                    , padding: '.75em'
+                    , borderRadius:`1.5em`
+                }).attr({ type:'submit', value:'PRONTO' }).on('click', function() {
                     const data = this.upFind('form').json() ;;
                     sock('auth/sign', { data, callback: res => {
                         if(res.status) {
                             app.storage('uat', res.uat)
                             app.success('Login realizado com sucesso! Reiniciando o sistema...')
-                            setTimeout(_ => location.reload(), 2000)
+                            setTimeout(_ => location.reload(), AL*2)
                         } else {
                             app.storage('uat', "")
                             app.error('Ops! Algo deu errado, tente novamente mais tarde...')
@@ -30,22 +37,17 @@
                     } })
                 })
             )
-        ).app(
+        ,
             DIV('row content-center').app(
                 SPAN('OU', null, { color:'{{SILVER}}44', padding:'1em' })
             )
-        ).app(
+        ,
             DIV('row').app(
                 TAG('input', 'row', { background:'{{silver}}22', color:"{{silver}}" }).attr({ name:'token', type:'text', placeholder:'Chave' })
             )
-        )
-    )
+    ])
     ;;
 
-    $('#app').empty().css({ background: "{{midnight_blue}}", color:"{{silver" }).app(
-        TAG('header', 'row relative content-center', { height:'20vh' }).app(
-            SPAN('·.·.·L·O·G·I·N·.·.·', 'centered -roboto-thin invert', { fontSize:'3em' })
-        )
-    ).app(d)
+    $('#app').empty().css({ background: "{{foreground}}", color:"{{silver}}" }).app(d)
 
 })
